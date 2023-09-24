3ca0f6518e40839316eeb2ce0202c2e6e78d48c7
tinoj <tino.korpelainen@aalto.fi>
2023-09-24 13:28:46 +0300

    Update

--- src/downloader.py
+++ src/downloader.py
@@ -47,6 +47,13 @@ class Downloader:
     YTDL_PLAYLIST_OPTIONS = {
         "extract_flat": "in_playlist",
         "skip_download": True,
+        "nocheckcertificate": True,
+        "ignoreerrors": False,
+        "logtostderr": False,
+        "quiet": True,
+        "no_warnings": True,
+        "default_search": "auto",
+        "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
     }
 
     @classmethod
